a97f54f19c11477cc9f4f1e2774d9a2b11f65783
Matt <70668167+MattJhet@users.noreply.github.com>
2024-07-24 09:04:52 -0400

    added the error catch
    users are required to have text in the boxes before submitting

--- form.js
+++ form.js
@@ -7,19 +7,34 @@ const formEl = document.getElementById('awesomeForm');
 formEl.addEventListener('submit', (event) => {
     event.preventDefault();
     
+    // making variable for our values
     const titleValue = titleEl.value;
     const usernameValue = usernameEl.value;
     const contentValue = contentEl.value;
 
+    // storing as a object to make things more efficient
     const blogInfo = {
         username: usernameValue,
         title: titleValue,
         content: contentValue
     }
     
-    localStorage.setItem('title', titleValue);
-    localStorage.setItem('usernameEl', usernameEl);
-    localStorage.setItem('content', contentValue);
-    console.log('test');
-    localStorage.setItem('form', JSON.stringify(blogInfo));
+    // adding values to local storage
+    // localStorage.setItem('title', titleValue);
+    // localStorage.setItem('usernameEl', usernameValue);
+    // localStorage.setItem('content', contentValue);
+    // console.log('test');
+    // only need one local storage now
+    localStorage.setItem('blog', JSON.stringify(blogInfo));
+
+    //make sure text in the all the spots are required
+    if (usernameEl == '' || titleEl == '' || contentEl == '') {
+        window.alert('Error please enter the all text boxes');
+        return;
+        
+    }
+    alert('Error please enter the all text boxes');
+    
 });
+
+
