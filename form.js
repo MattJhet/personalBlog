d38cb0203104d5643dd2010987df06d255d8fbf7
Matt <70668167+MattJhet@users.noreply.github.com>
2024-07-24 08:43:17 -0400

    fixed errors

--- form.js
+++ form.js
@@ -1,24 +1,25 @@
-const usernameEl = document.getElementById('username');
+const usernameEl = document.getElementById('person');
 const titleEl = document.getElementById('title');
-const contentEl = document.getElementById('content')
+const contentEl = document.getElementById('content');
 const formEl = document.getElementById('awesomeForm');
 
-formEl.addEventListener('sumbit', (event) => {
+// fixed typo error
+formEl.addEventListener('submit', (event) => {
     event.preventDefault();
     
     const titleValue = titleEl.value;
     const usernameValue = usernameEl.value;
-    const contentValue = contentValue.el;
+    const contentValue = contentEl.value;
 
     const blogInfo = {
         username: usernameValue,
         title: titleValue,
         content: contentValue
     }
-
-    // localStorage.setItem('title', titleValue);
-    // localStorage.setItem('usernameEl', usernameEl);
-    // localStorage.setItem('content', contentValue);
-
+    
+    localStorage.setItem('title', titleValue);
+    localStorage.setItem('usernameEl', usernameEl);
+    localStorage.setItem('content', contentValue);
+    console.log('test');
     localStorage.setItem('form', JSON.stringify(blogInfo));
-})
+});
